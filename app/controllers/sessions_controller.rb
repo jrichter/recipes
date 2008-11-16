4a273b02477f8bec7b679f74a6ab464f8360c3ec
Justin Richter <jrichter@jetfive.com>
2008-11-15 23:36:41 -0600

    streamline auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,7 @@ class SessionsController < ApplicationController
   
   #GET
   def new
-#    redirect_to 'https://www.google.com/accounts/AuthSubRequest?scope=http%3A%2F%2Fwww.google.com%2Fcalendar%2Ffeeds%2F&session=1&secure=1&next=http%3A%2F%2Fwww.jetfive.com/login'
-    redirect_to 'https://www.google.com/accounts/AuthSubRequest?next=http%3A%2F%2Fwww.jetfive.com%2Flogin&scope=http%3A%2F%2Fwww.google.com%2Fcalendar%2Ffeeds%2F&session=1&secure=1'
-    
+    @session = Session.new
   end
   
   #POST
@@ -15,21 +13,31 @@ class SessionsController < ApplicationController
   end
 
   def login
-    token = params[:token] 
+    if params[:token]
+      token = params[:token] 
 
-    http = Net::HTTP.new('www.google.com', 443)
-    http.use_ssl = true
-    path = '/accounts/AuthSubTokenInfo'
-    full_path = 'https://www.google.com' + path
-    headers = google_header(full_path, token)
+      http = Net::HTTP.new('www.google.com', 443)
+      http.use_ssl = true
+      path = '/accounts/AuthSubTokenInfo'
+      full_path = 'https://www.google.com' + path
+      headers = google_header(full_path, token)
 
-    resp, data = http.get(path, headers)
+      resp, data = http.get(path, headers)
 
-    @header = headers
-    @code =  'Code = ' + resp.code
-    @msg =  'Message = ' + resp.message
-    @data = data
-    @token = token
+      @header = headers
+      @code =  'Code = ' + resp.code
+      @msg =  'Message = ' + resp.message
+      @data = data
+      @token = token
+      
+      if resp.message == "OK"
+        @session = Session.new(:ip_addr => request.remote_addr, :logged_in => true)
+      else
+        flash[:notice] = "Something failed with Google authentication."
+      end
+    else
+      redirect_to 'https://www.google.com/accounts/AuthSubRequest?next=http%3A%2F%2Fwww.jetfive.com%2Flogin&scope=http%3A%2F%2Fwww.google.com%2Fcalendar%2Ffeeds%2F&session=1&secure=1'    
+    end
   end
 
   def google_header url, token
