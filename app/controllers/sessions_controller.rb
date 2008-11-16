41344c880a48f470cb6bda61dc2fda719f54f076
Justin Richter <jrichter@jetfive.com>
2008-11-15 22:41:24 -0600

    check headers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   #GET
   def new
 #    redirect_to 'https://www.google.com/accounts/AuthSubRequest?scope=http%3A%2F%2Fwww.google.com%2Fcalendar%2Ffeeds%2F&session=1&secure=1&next=http%3A%2F%2Fwww.jetfive.com/login'
-    redirect_to 'https://www.google.com/accounts/AuthSubRequest?next=http%3A%2F%2Fwww.jetfive.com%2login&scope=http%3A%2F%2Fwww.google.com%2Fcalendar%2Ffeeds%2F&session=1&secure=1'
+    redirect_to 'https://www.google.com/accounts/AuthSubRequest?next=http%3A%2F%2Fwww.jetfive.com%2Flogin&scope=http%3A%2F%2Fwww.google.com%2Fcalendar%2Ffeeds%2F&session=1&secure=1'
     
   end
   
@@ -25,6 +25,7 @@ class SessionsController < ApplicationController
 
     resp, data = http.get(path, headers)
 
+    @header = headers
     @code =  'Code = ' + resp.code
     @msg =  'Message = ' + resp.message
     @data = data
