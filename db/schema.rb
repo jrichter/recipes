ba877f934dba3a7e447825b5a5f8504d5d0518b4
Justin Richter <jrichter@jetfive.com>
2009-01-02 04:39:07 +0000

    grr

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
+# please use the migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -9,12 +9,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081115041627) do
+ActiveRecord::Schema.define(:version => 2147483647) do
 
   create_table "amounts", :force => true do |t|
     t.string   "ing_amnt"
-    t.integer  "ingredient_id", :limit => 11
-    t.integer  "recipe_id",     :limit => 11
+    t.integer  "ingredient_id"
+    t.integer  "recipe_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(:version => 20081115041627) do
   create_table "recipes", :force => true do |t|
     t.string   "name"
     t.string   "author"
-    t.integer  "oven_temp",  :limit => 11
+    t.integer  "oven_temp"
     t.text     "directions"
     t.datetime "created_at"
     t.datetime "updated_at"
