bfb970e4734913bf54c0f3d5108194588beb3d64
jrichter <jrichter@jetfive.com>
2010-01-11 22:33:51 -0600

    fix flickr_feed for ruby1.9

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,7 +11,7 @@ in_place_editable_columns :recipe, :picture_url
 
   def self.flickr_feed(rssfeed="http://api.flickr.com/services/feeds/groups_pool.gne?id=78103915@N00&lang=en-us&format=rss_200")
     feed = Feedzirra::Feed.fetch_and_parse(rssfeed)
-    image_url = feed.entries[rand(feed.entries.length)].summary.scan(/(<a.*<\/a>)/).last.to_s.gsub(/height=\"\d\d\d\"/, "height=\"172\"").gsub(/width=\"\d\d\d\"/, "width=\"233\"")
+    image_url = feed.entries[rand(feed.entries.length)].summary.scan(/(<a.*<\/a>)/).last[0].gsub(/height=\"\d\d\d\"/, "height=\"172\"").gsub(/width=\"\d\d\d\"/, "width=\"233\"")
     if image_url.scan(/<img src=/) == []
       self.flickr_feed
     else
